4e1a01b16125d8c0b13a4bc9959d778b27b53cfc
Shra1V32 <namanageshwar@outlook.com>
2023-03-21 12:31:28 +0530

    CommandLineArguments: Reformat code

--- CommandLineArguments.java
+++ CommandLineArguments.java
@@ -2,9 +2,9 @@ public class CommandLineArguments {
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
     }
 }
